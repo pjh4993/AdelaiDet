d6e48668edc9a7bb38d169a9e541406bc5b0b3a3
junho park <pjh3974@gmail.com>
2020-12-02 12:48:11 +0000

    clean identity loss from fcos

--- adet/modeling/fcos/fcos.py
+++ adet/modeling/fcos/fcos.py
@@ -57,9 +57,9 @@ class FCOS(nn.Module):
 
     def forward_head(self, features, top_module=None):
         features = [features[f] for f in self.in_features]
-        pred_class_logits, pred_deltas, pred_centerness, top_feats, bbox_towers, identity = self.fcos_head(
+        pred_class_logits, pred_deltas, pred_centerness, top_feats, bbox_towers = self.fcos_head(
             features, top_module, self.yield_proposal)
-        return pred_class_logits, pred_deltas, pred_centerness, top_feats, bbox_towers, identity
+        return pred_class_logits, pred_deltas, pred_centerness, top_feats, bbox_towers
 
     def forward(self, images, features, gt_instances=None, top_module=None):
         """
@@ -76,7 +76,7 @@ class FCOS(nn.Module):
         """
         features = [features[f] for f in self.in_features]
         locations = self.compute_locations(features)
-        logits_pred, reg_pred, ctrness_pred, identity, top_feats, bbox_towers = self.fcos_head(
+        logits_pred, reg_pred, ctrness_pred, top_feats, bbox_towers = self.fcos_head(
             features, top_module, self.yield_proposal
         )
 
@@ -88,7 +88,7 @@ class FCOS(nn.Module):
 
         if self.training:
             results, losses = self.fcos_outputs.losses(
-                logits_pred, reg_pred, ctrness_pred, identity,
+                logits_pred, reg_pred, ctrness_pred,
                 locations, gt_instances, top_feats,
             )
             
@@ -135,8 +135,6 @@ class FCOSHead(nn.Module):
                         "bbox": (cfg.MODEL.FCOS.NUM_BOX_CONVS,
                                  cfg.MODEL.FCOS.USE_DEFORMABLE),
                         "share": (cfg.MODEL.FCOS.NUM_SHARE_CONVS,
-                                  False),
-                        "identity": (cfg.MODEL.FCOS.NUM_ID_CONVS,
                                   False),
                         }
         norm = None if cfg.MODEL.FCOS.NORM == "none" else cfg.MODEL.FCOS.NORM
@@ -191,10 +189,6 @@ class FCOSHead(nn.Module):
             in_channels, 1, kernel_size=3,
             stride=1, padding=1
         )
-        self.identity = nn.Conv2d(
-            in_channels, self.id_dim, kernel_size=3,
-            stride=1, padding=1
-        )
 
         if cfg.MODEL.FCOS.USE_SCALE:
             self.scales = nn.ModuleList([Scale(init_value=1.0) for _ in range(self.num_levels)])
@@ -203,8 +197,8 @@ class FCOSHead(nn.Module):
 
         for modules in [
             self.cls_tower, self.bbox_tower,
-            self.share_tower, self.identity_tower,
-            self.cls_logits, self.bbox_pred, self.ctrness, self.identity,
+            self.share_tower,
+            self.cls_logits, self.bbox_pred, self.ctrness
         ]:
             for l in modules.modules():
                 if isinstance(l, nn.Conv2d):
@@ -222,19 +216,16 @@ class FCOSHead(nn.Module):
         logits = []
         bbox_reg = []
         ctrness = []
-        identity = []
         top_feats = []
         bbox_towers = []
         for l, feature in enumerate(x):
             feature = self.share_tower(feature)
             cls_tower = self.cls_tower(feature)
             bbox_tower = self.bbox_tower(feature)
-            identity_tower = self.identity_tower(feature)
             if yield_bbox_towers:
                 bbox_towers.append(bbox_tower)
 
             logits.append(self.cls_logits(cls_tower))
-            identity.append(self.identity(identity_tower).sigmoid())
 
             if self.ctrness_on_bbox:
                 ctrness.append(self.ctrness(bbox_tower))
@@ -249,4 +240,4 @@ class FCOSHead(nn.Module):
             if top_module is not None:
                 top_feats.append(top_module(bbox_tower))
         
-        return logits, bbox_reg, ctrness, identity, top_feats, bbox_towers, 
+        return logits, bbox_reg, ctrness, top_feats, bbox_towers, 
